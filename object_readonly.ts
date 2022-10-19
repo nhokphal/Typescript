47cfde644896f64e83b18200f75f41b5f0fdab6e
nhokphal <phalnhok@gmail.com>
2022-10-19 15:03:38 +0700

    add object type

---
+++ object_readonly.ts
@@ -0,0 +1,13 @@
+
+// readypmy
+
+interface setType {
+    readonly id_readony: number;
+    name_readonly: number;
+}
+
+const type: setType = {
+    id_readony: 23,
+    name_readonly: 23,
+}
+console.log(type)
